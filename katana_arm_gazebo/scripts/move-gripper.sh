5adccf27d0d66bba90a9a66c2cc65d4d3ecaf5ec
Martin Günther <mguenthe@uos.de>
2011-08-31 13:56:44 +0200

    adapted test scripts to new gripper controller

--- katana_arm_gazebo/scripts/move-gripper.sh
+++ katana_arm_gazebo/scripts/move-gripper.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-rostopic pub -1 /katana_r_finger_position_controller/command std_msgs/Float64 0.3 &
-rostopic pub -1 /katana_l_finger_position_controller/command std_msgs/Float64 0.3 &
 
-sleep 10
+# close gripper
+# GRASP=2
+rostopic pub -1 /gripper_grasp_posture_controller/goal \
+  object_manipulation_msgs/GraspHandPostureExecutionActionGoal -- \
+  '{header: auto, goal: {grasp: {grasp_posture: {position: [0.0]}}, goal: 2}}'
 
-rostopic pub -1 /katana_r_finger_position_controller/command std_msgs/Float64 -- -0.4 &
-rostopic pub -1 /katana_l_finger_position_controller/command std_msgs/Float64 -- -0.4 &
+# open gripper
+# RELEASE=3
+rostopic pub -1 /gripper_grasp_posture_controller/goal \
+  object_manipulation_msgs/GraspHandPostureExecutionActionGoal -- \
+  '{header: auto, goal: {goal: 3}}'
 
+# move gripper to angle 0.0
+# PRE_GRASP=1
+rostopic pub -1 /gripper_grasp_posture_controller/goal \
+  object_manipulation_msgs/GraspHandPostureExecutionActionGoal -- \
+  '{header: auto, goal: {grasp: {grasp_posture: {position: [0.0]}, pre_grasp_posture: {position: [0.0]}}, goal: 1}}'
