b1f237bd7598253e4c2b7b08ccf0da1ebf06d46f
Martin Günther <mguenthe@uos.de>
2013-11-27 17:55:59 +0100

    katana: remove acceleration discontinuity warning
    The trajectories generated by arm_navigation and MoveIt have
    acceleration discontinuities, and that's fine, so this warning has to be
    removed. As far as I can tell, the constraints are:
    
    * positions have to be continuous and differentiable
    * velocities have to be continuous
    * accelerations don't have to be either

--- katana/src/joint_trajectory_action_controller.cpp
+++ katana/src/joint_trajectory_action_controller.cpp
@@ -721,11 +721,6 @@ bool JointTrajectoryActionController::validTrajectory(const SpecifiedTrajectory 
         ROS_ERROR("Velocity discontinuity at end of segment %zu (joint %zu)", seg, j);
         return false;
       }
-      if (std::abs(2.0 * traj[seg + 1].splines[j].coef[2] - acc_t) > EPSILON)
-      {
-        ROS_WARN("Acceleration discontinuity (old segment: %f, new segment: %f, diff: %f) at end of segment %zu (joint %zu)", 2.0 * traj[seg + 1].splines[j].coef[2], acc_t, std::abs(2.0 * traj[seg + 1].splines[j].coef[2] - acc_t), seg, j);
-        // return false;
-      }
     }
   }
 
