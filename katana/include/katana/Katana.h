8f6bf4934863b978c1b8bf4a73cd04df03e0bbfb
Martin Günther <mguenthe@uos.de>
2014-02-03 20:58:47 +0100

    Katana300: override executeTrajectory() implementation
    This implementation is based on kni->sendSplineToMotor(), which is
    supported by the Katana 300 (in contrast to the
    setAndStartPolyMovement() used with the Katana450).
    
    closes #6
    
    original author: Benjamin Reiner <reinerbe@hs-weingarten.de>

--- katana/include/katana/Katana.h
+++ katana/include/katana/Katana.h
@@ -74,6 +74,8 @@ protected:
 
   KNIConverter* converter;
 
+  short round(const double x);
+
 private:
   ros::ServiceServer switch_motors_off_srv_;
   ros::ServiceServer switch_motors_on_srv_;
@@ -89,8 +91,6 @@ private:
   bool switchMotorsOff(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response);
   bool switchMotorsOn(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response);
   bool testSpeedSrv(std_srvs::Empty::Request &request, std_srvs::Empty::Response &response);
-
-  short round(const double x);
 };
 
 }
